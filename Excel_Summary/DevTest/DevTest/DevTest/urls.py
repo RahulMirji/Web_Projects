e2dc2e0daa778686702719e4a06737120925b775
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-10-19 23:47:50 +0530

    ai chat bot

--- Excel_Summary/DevTest/DevTest/DevTest/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('file_upload.urls')),  # <-- Include the app's URLs
-]
--- Excel_Summary/DevTest/DevTest/file_upload/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.upload_file, name='upload_file'),
-    path('summary/', views.summary, name='summary'),
-]
